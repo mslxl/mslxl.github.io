04542afef428798ba2b7210b7a93070dd2207af2
Mslxl <1302485739@qq.com>
2018-08-15 00:02:17 +0800

    Switch to https

---
+++ deploy.bat
@@ -0,0 +1,18 @@
+@echo off
+choice /m "Are you sure to deploy?"
+
+if %errorlevel%==2 exit
+
+cmd /c hexo clean
+cmd /c hexo g
+git add .
+git status
+
+choice /m "Are you sure to commit these file?"
+if %errorlevel%==2 exit
+
+set /p msg=Commit msg:
+git commit -m "%msg%"
+
+git push origin blog-source
+
