08f27ee18c4df5554710768d3eefe1317f58739a
Mslxl <1302485739@qq.com>
2018-08-21 16:44:20 +0800

    Update

--- deploy.bat
+++ deploy.bat
@@ -3,8 +3,15 @@ choice /m "Are you sure to deploy?"
 
 if %errorlevel%==2 exit
 
+if not exist "node_modules" (
+	cmd /c npm install -g hexo hexo-cli
+	cmd /c npm install
+)
+ 
 cmd /c hexo clean
 cmd /c hexo g
+git pull
+cls
 git add .
 git status
 
@@ -14,5 +21,13 @@ if %errorlevel%==2 exit
 set /p msg=Commit msg:
 git commit -m "%msg%"
 
+cls
+echo Pushing...
 git push origin source
 
+if %errorlevel%==0 (
+	echo successful
+)else(
+	echo failed
+)
+pause >nul
