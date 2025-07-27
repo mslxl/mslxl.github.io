0c3dae1b5f51987ddcf360eb37b805015cba1956
mslxl <i@mslxl.com>
2025-07-27 16:30:23 +0800

    chore: claim folo

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -4,4 +4,10 @@ export const userConfig: Partial<UserConfig> = {
   // Override the default config here
   // site: { title: "講評世界" },
   // seo: { twitter: "@moeyua13" },
+  rss: {
+    follow: {
+      feedId: '62058027190779939',
+      userId: '83061347508956160',
+    },
+  },
 }
